f8fef642a87d49e1c2881f4c8e064ea2cae94ca6
Hamzaoui-Louai <l_hamzaoui@estin.dz>
2026-08-27 15:05:58 +0100

    added offscreener logic

---
+++ FALL/Assets/Scripts/Level Environment/CameraBehaviour.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    Transform transform;
+    Transform playerTransform;
+    [SerializeField] float upperLimit;
+    [SerializeField] float lowerLimit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ FALL/Assets/Scripts/Level Environment/Offscreener.cs
@@ -0,0 +1,76 @@
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class Offscreener : MonoBehaviour
+{
+    Transform transform;
+
+    Transform playerTransform;
+
+    [SerializeField] float speed = 1f;
+
+    float pauseTimer = 0f;
+
+    const float FrameRate = 60f;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        if(IsPaused())
+        {
+            TickPauseTimer();
+        }
+        else
+        {
+            SeekPlayer();
+        }
+    }
+
+    public void PauseOffscreener(int pauseDuration) //duration is in secondes while pause timer ticks per frame 
+    {
+        pauseTimer = pauseDuration * FrameRate;
+    }
+
+    public void SetOffscreenerSpeed(float newOffScreenerSpeed)
+    {
+        speed = newOffScreenerSpeed;
+    }
+
+    public void SetOffScreenerYPosition(float newOffscreenerYPosition)
+    {
+        transform.position = new Vector2(0, newOffscreenerYPosition);
+    }
+
+    void TickPauseTimer()
+    {
+        pauseTimer--;
+    }
+
+    bool IsPaused()
+    {
+        return pauseTimer > 0;
+    }
+
+    void SeekPlayer()
+    {
+        if(ReachedPlayer())
+        {
+            //game over
+        }
+        else
+        {
+            transform.position = new Vector2(0, transform.position.y - speed / FrameRate);
+        }
+    }
+
+    bool ReachedPlayer()
+    {
+        const float OffscreenerOffset = 7.5f;
+        return (playerTransform.position.y >= transform.position.y + OffscreenerOffset);
+    }
+}
